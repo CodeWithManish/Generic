a60f6dffa24e23b2dfadbfe2704bc334874cb667
Manish <manishkumar0156@gmail.com>
2022-02-09 14:45:27 +0530

    [Manish] Add: print Array sample code using Generic

---
+++ GenericDemo/GenericOne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class GenericOne
+    {
+        public void Print<T>(T[] Array)
+        {
+            foreach (var item in Array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n==============\n");
+        }
+    }
+}
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericOne gen = new GenericOne();
+            int[] intArr = { 1, 2, 3, 4, 5 };
+            double[] doubleArr = { 1.3, 2.1, 3.1 };
+            char[] charArr = { 'A', 'B', 'C' };
+            gen.Print(intArr);
+            gen.Print(doubleArr);
+            gen.Print(charArr);
+            Console.ReadKey();
+        }
+    }
+}
