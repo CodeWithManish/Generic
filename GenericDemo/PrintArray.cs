6cc5888804e3bef9db9b98a933f6cda19e61f204
Manish <manishkumar0156@gmail.com>
2022-02-09 15:25:55 +0530

    [Manish] Add: added generic class to print Array

--- GenericDemo/PrintArray.cs
+++ GenericDemo/PrintArray.cs
@@ -6,15 +6,24 @@ using System.Threading.Tasks;
 
 namespace GenericDemo
 {
-    public class GenericOne
+    public class PrintArray<T>
     {
-        public void Print<T>(T[] Array)
+       /* public void Print<T>(T[] Array)
         {
             foreach (var item in Array)
             {
                 Console.WriteLine(item);
             }
+        }*/
+        public void printArr(T[] Array)
+        {
+            foreach (T item in Array)
+            {
+                Console.WriteLine(item);
+            }
             Console.WriteLine("\n==============\n");
         }
+
+        
     }
 }
--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -10,13 +10,23 @@ namespace GenericDemo
     {
         static void Main(string[] args)
         {
-            GenericOne gen = new GenericOne();
+            //GenericOne gen = new GenericOne();
             int[] intArr = { 1, 2, 3, 4, 5 };
             double[] doubleArr = { 1.3, 2.1, 3.1 };
             char[] charArr = { 'A', 'B', 'C' };
-            gen.Print(intArr);
+           /* gen.Print(intArr);
             gen.Print(doubleArr);
-            gen.Print(charArr);
+            gen.Print(charArr);*/
+
+            PrintArray<int> printInt = new PrintArray<int>();
+            printInt.printArr(intArr);
+
+            PrintArray<double> printD = new PrintArray<double>();
+            printD.printArr(doubleArr);
+
+            PrintArray<char> printChar = new PrintArray<char>();
+            printChar.printArr(charArr);  
+
             Console.ReadKey();
         }
     }
