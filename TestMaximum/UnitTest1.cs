bb0e17d418bbfbadfa2d0a9824932516f9328a98
Manish <manishkumar0156@gmail.com>
2022-02-11 14:42:16 +0530

    [Manish] Add: Find Three Float maximum number

--- TestMaximum/UnitTest1.cs
+++ TestMaximum/UnitTest1.cs
@@ -28,9 +28,29 @@ namespace TestMaximum
             Assert.AreEqual(3, max);
         }
 
-      
+        //for Float Number
+        public void GivenFloatValuesFirstPosition_whenCheck_ShouldReturnMaxNum()
+        {
+            float max = findMaxNumber.FloatMaxValueOfThree(1.2f, 2.1f, 3.0f);
+            Assert.AreEqual(3.0, max);
+        }
 
-       
+        [Test]
+        public void GivenFloatValuesSecondPosition_whenCheck_ShouldReturnMaxNum()
+        {
+            float max = findMaxNumber.FloatMaxValueOfThree(2.1f, 3.0f, 1.2f);
+            Assert.AreEqual(3.0, max);
+        }
+
+        [Test]
+        public void GivenFloatValuesThirdPosition_whenCheck_ShouldReturnMaxNum()
+        {
+            float max = findMaxNumber.FloatMaxValueOfThree(3.0f, 2.1f, 1.2f);
+            Assert.AreEqual(3.0, max);
+        }
+
+
+
 
     }
 }
