fe957e905c1a75ff99b1df4a39bdbfeb4cad4a1c
Manish <manishkumar0156@gmail.com>
2022-02-11 16:29:26 +0530

    [Manish] Add: Three maximum string

--- MaximumNumber/FindMaxNumber.cs
+++ MaximumNumber/FindMaxNumber.cs
@@ -40,5 +40,21 @@ namespace MaximumNumber
                 return thirdNumber;
             }
         }
+
+        public string StringMaxValueOfThree( string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0)
+            {
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0)
+            {
+                return secondString;
+            }
+            else
+            {
+                return thirdString;
+            }
+        }
     }
 }
--- TestMaximum/UnitTest1.cs
+++ TestMaximum/UnitTest1.cs
@@ -5,48 +5,70 @@ namespace TestMaximum
 {
     public class Tests
     {
-        FindMaxNumber findMaxNumber = new FindMaxNumber();
+        FindMaxNumber findMax = new FindMaxNumber();
+        
 
         [Test]
         public void GivenIntegerValuesFirstPosition_whenCheck_ShouldReturnMaxNum()
         {
-            int max = findMaxNumber.FindMaxValueOfThree(1, 2, 3);
+            int max = findMax.FindMaxValueOfThree(1, 2, 3);
             Assert.AreEqual(3, max);
         }
 
         [Test]
         public void GivenIntegerValuesSecondPosition_whenCheck_ShouldReturnMaxNum()
         {
-            int max = findMaxNumber.FindMaxValueOfThree(2, 3, 1);
+            int max = findMax.FindMaxValueOfThree(2, 3, 1);
             Assert.AreEqual(3, max);
         }
 
         [Test]
         public void GivenIntegerValuesThirdPosition_whenCheck_ShouldReturnMaxNum()
         {
-            int max = findMaxNumber.FindMaxValueOfThree(3, 2, 1);
+            int max = findMax.FindMaxValueOfThree(3, 2, 1);
             Assert.AreEqual(3, max);
         }
 
         //for Float Number
+        [Test]
         public void GivenFloatValuesFirstPosition_whenCheck_ShouldReturnMaxNum()
         {
-            float max = findMaxNumber.FloatMaxValueOfThree(1.2f, 2.1f, 3.0f);
+            float max = findMax.FloatMaxValueOfThree(3.0f, 1.2f, 2.1f);
             Assert.AreEqual(3.0, max);
         }
 
         [Test]
         public void GivenFloatValuesSecondPosition_whenCheck_ShouldReturnMaxNum()
         {
-            float max = findMaxNumber.FloatMaxValueOfThree(2.1f, 3.0f, 1.2f);
+            float max = findMax.FloatMaxValueOfThree(2.1f, 3.0f, 1.2f);
             Assert.AreEqual(3.0, max);
         }
 
         [Test]
         public void GivenFloatValuesThirdPosition_whenCheck_ShouldReturnMaxNum()
         {
-            float max = findMaxNumber.FloatMaxValueOfThree(3.0f, 2.1f, 1.2f);
+            float max = findMax.FloatMaxValueOfThree(3.0f, 2.1f, 1.2f);
             Assert.AreEqual(3.0, max);
+        }
+
+        /*For String */
+        [Test]
+        public void GivenStringValuesFirstPosition_whenCheck_ShouldReturnMaxString()
+        {
+            string max = findMax.StringMaxValueOfThree("Peach", "Apple", "Banana");
+            Assert.AreEqual("Peach", max);
+        }
+        [Test]
+        public void GivenStringValuesSecondPosition_whenCheck_ShouldReturnMaxString()
+        {
+            string max = findMax.StringMaxValueOfThree("Apple", "Peach", "Banana");
+            Assert.AreEqual("Peach", max);
+        }
+        [Test]
+        public void GivenStringValuesThirdPosition_whenCheck_ShouldReturnMaxString()
+        {
+            string max = findMax.StringMaxValueOfThree("Apple", "Banana", "Peach");
+            Assert.AreEqual("Peach", max);
         }
 
 
